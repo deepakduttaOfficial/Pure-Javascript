03069a3fbc02bcd22f653b11d72abec36997d112
Deepak Dutta <coder.deepakyt@gmail.com>
2023-01-23 06:40:04 -0700

    07QRCodeGenerator - API has been created

---
+++ 07QRCodeGenerator - API/App.js
@@ -0,0 +1,20 @@
+const button = document.getElementById("submit");
+const main = document.getElementById("main");
+const div = document.createElement("div");
+main.appendChild(div);
+
+button.addEventListener("click", generateQRCode);
+
+function generateQRCode() {
+  let website = document.getElementById("input").value;
+  div.innerHTML = "";
+  if (website) {
+    new generateQRCode(div, {
+      text: website,
+      width: 128,
+      height: 128,
+    });
+  } else {
+    alert("Please enter a valid URL");
+  }
+}
